3e0dd5f2335dc720e3876f3f05f8fa60964d016f
SenthilKumar <senthilkumar.m@nexware-global.com>
2020-03-28 11:12:24 +0530

    Home screen changes

--- app/src/main/java/com/example/coronaaware/ui/ui/VerifyPhoneActivity.java
+++ app/src/main/java/com/example/coronaaware/ui/ui/VerifyPhoneActivity.java
@@ -194,7 +194,7 @@ public class VerifyPhoneActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                
+
             }
         });
     }
